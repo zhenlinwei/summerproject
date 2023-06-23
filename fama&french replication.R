ddd83446e64486b6a66d4e2db9689195c1aaa426
zhenlinwei <137361465+zhenlinwei@users.noreply.github.com>
2023-06-23 18:22:55 -0400

    Update fama&french replication.R

--- fama&french replication.R
+++ fama&french replication.R
@@ -7,7 +7,7 @@ financial_data$annDate = as.Date(financial_data$annDate)
 market_data$Date = as.Date(market_data$Date)
 financial_data$monthly = floor_date(financial_data$annDate, unit = "quarter")
 market_data$monthly = make_date(year(market_data$Date), month(market_data$Date))
-merged_data = merge(financial_data, marketdata, by = c("id","monthly"))
+merged_data = merge(financial_data, market_data, by = c("id","monthly"))
 library(tidyr)
 merged_data <- merged_data %>%
   group_by(id) %>%
