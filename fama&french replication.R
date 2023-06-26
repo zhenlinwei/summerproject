1e69bf25f6633d03f061462deccf1c9cf3af34b1
zhenlinwei <137361465+zhenlinwei@users.noreply.github.com>
2023-06-26 03:54:11 -0400

    Update fama&french replication.R

--- fama&french replication.R
+++ fama&french replication.R
@@ -1,3 +1,4 @@
+#1
 library(readxl)
 financial_data = read_excel("financialdata excel.xlsx")
 market_data = read_excel("marketdata excel.xlsx")
@@ -22,4 +23,34 @@ merged_row = merge(financial_row, market_data[i, ], by = "id")
 merged_data = rbind(merged_data, merged_row)
 
 rownames(merged_data) = NULL
+print(merged_data)
+
+
+#2
+library(readxl)
+financial_data = read_excel("financialdata excel.xlsx")
+market_data = read_excel("marketdata excel.xlsx")
+library(dplyr)
+library(zoo)
+library(data.table)
+library(lubridate)
+
+financial_data$annDate = as.Date(financial_data$annDate)
+market_data$Date = as.Date(market_data$Date)
+
+merged_data = data.frame()
+for (i in 1:nrow(market_data)) {
+  id = market_data$id[i]
+  specific_date = market_data$Date[i]
+  
+  close_dates = financial_data$annDate[financial_data$id == id & financial_data$annDate <= specific_date]
+  
+  if (length(close_dates) > 0) {
+    closest_date = max(close_dates)
+    closest_book = financial_data$book[financial_data$id == id & financial_data$annDate == closest_date]
+    
+    merged_data <- rbind(merged_data, data.frame(id = id, date = specific_date, closest_date = closest_date, book = closest_book))
+  }
+}
+
 print(merged_data)
