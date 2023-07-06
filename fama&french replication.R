8fb98a231cd017185ab576b48189f69f09bafdea
zhenlinwei <137361465+zhenlinwei@users.noreply.github.com>
2023-07-06 17:54:44 -0400

    Update fama&french replication.R

--- fama&french replication.R
+++ fama&french replication.R
@@ -1,84 +1,29 @@
-#1
 library(readxl)
-financial_data = read_excel("financialdata excel.xlsx")
-market_data = read_excel("marketdata excel.xlsx")
 library(dplyr)
 library(zoo)
 library(data.table)
 library(lubridate)
 
-financial_data$annDate = as.Date(financial_data$annDate)
-market_data$Date = as.Date(market_data$Date)
-financial_data = financial_data[order(financial_data$annDate, decreasing = TRUE), ]
-
-merged_data = data.frame()
-for (i in 1:nrow(market_data)) {
-  id = market_data$id[i]
-  date = market_data$Date[i]
-}
-closest_date = financial_data$annDate[financial_data$id == id][which.max(financial_data$annDate[financial_data$id == id] <= date)]
-financial_row = financial_data[financial_data$id == id & financial_data$annDate == closest_date, ]
-merged_row = merge(financial_row, market_data[i, ], by = "id")
-
-merged_data = rbind(merged_data, merged_row)
-
-rownames(merged_data) = NULL
-print(merged_data)
-
-
-#2
-library(readxl)
 financial_data = read_excel("financialdata excel.xlsx")
 market_data = read_excel("marketdata excel.xlsx")
-library(dplyr)
-library(zoo)
-library(data.table)
-library(lubridate)
-
-financial_data$annDate = as.Date(financial_data$annDate)
-market_data$Date = as.Date(market_data$Date)
-
-merged_data = data.frame()
-for (i in 1:nrow(market_data)) {
-  id = market_data$id[i]
-  specific_date = market_data$Date[i]
-  
-  close_dates = financial_data$annDate[financial_data$id == id & financial_data$annDate <= specific_date]
-  
-  if (length(close_dates) > 0) {
-    closest_date = max(close_dates)
-    closest_book = financial_data$book[financial_data$id == id & financial_data$annDate == closest_date]
-    
-    merged_data <- rbind(merged_data, data.frame(id = id, date = specific_date, closest_date = closest_date, book = closest_book))
-  }
-}
-
-print(merged_data)
-
-#3
-library(data.table)
-library(lubridate)
-library(readxl)
-
 financial_data = as.data.table(read_excel("financialdata excel.xlsx"))
 market_data = as.data.table(read_excel("marketdata excel.xlsx"))
 
 financial_data[, annDate := as.Date(annDate)]
 market_data[, Date := as.Date(Date)]
 
-# treat quarterly reports and annual reports of a company announced on the same date 
 financial_data = financial_data[, rankFdate := rank(-as.numeric(fdate)), by = .(id, annDate)][rankFdate == 1]
-# merge financial and market data
+
 financial_data[, Date := annDate]
 merged_data = financial_data[market_data, on = .(id, Date), roll = Inf]
 
-# date check
-merged_data[annDate > Date] # if it returns any row, then something is wrong
-# number of entries per date check
-merged_data[,.N, by = .(id, Date)][N>1] # if it returns any row, then something is wrong
+merged_data[,.(Date, id, ret, meTotal, book)]
+june_data = merged_data[month(Date) == 6]
+june_data[, ":="(wt = meTotal/sum(meTotal, na.rm = TRUE)), by = Date]
+june_data[,.(Date, id, ret, meTotal, book, wt)]
 
-#Calculate the sum of meTotal on 6-30 of each year
-market_data$Year = format(market_data$Date, "%Y")
-market_data$Month = format(market_data$Date, "%m")
-june30_market_data = market_data[market_data$Month == "06" & market_data$Date == as.Date(paste0(market_data$Year, "-06-30")), ]
-sum_meTotal = sum(june30_market_data$meTotal,na.rm = TRUE)
+merged_data = merged_data[june_data[, .(Date, id, wt)], on = .(Date, id)][, .(Date, id, ret, meTotal, book, wt)]
+merged_data[, ":="(lag_wt = lag(wt, 1)), by = id]
+merged_data[, ":="(drifted_wt = lag_wt*(1+ret))]
+merged_data[, ":="(normal_drifted_wt = drifted_wt/sum(drifted_wt, na.rm = TRUE)), by = Date]
+merged_data[, ":="(wt = ifelse(!is.na(wt), wt, normal_drifted_wt))]
