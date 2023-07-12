ff22bc9ccdef9ad59c696402a244cdbb69e866f0
zhenlinwei <137361465+zhenlinwei@users.noreply.github.com>
2023-07-12 03:08:36 -0400

    Update fama&french replication.R

--- fama&french replication.R
+++ fama&french replication.R
@@ -22,8 +22,37 @@ june_data = merged_data[month(Date) == 6]
 june_data[, ":="(wt = meTotal/sum(meTotal, na.rm = TRUE)), by = Date]
 june_data[,.(Date, id, ret, meTotal, book, wt)]
 
-merged_data = merged_data[june_data[, .(Date, id, wt)], on = .(Date, id)][, .(Date, id, ret, meTotal, book, wt)]
-merged_data[, ":="(lag_wt = lag(wt, 1)), by = id]
-merged_data[, ":="(drifted_wt = lag_wt*(1+ret))]
-merged_data[, ":="(normal_drifted_wt = drifted_wt/sum(drifted_wt, na.rm = TRUE)), by = Date]
-merged_data[, ":="(wt = ifelse(!is.na(wt), wt, normal_drifted_wt))]
+new_merged_data = merged_data[june_data[, .(Date, id, wt)], on = .(Date, id)][, .(Date, id, ret, meTotal, book, wt)]
+new_merged_data[, ":="(lag_wt = lag(wt, 1)), by = id]
+new_merged_data[, ":="(drifted_wt = lag_wt*(1+ret))]
+new_merged_data[, ":="(normal_drifted_wt = drifted_wt/sum(drifted_wt, na.rm = TRUE)), by = Date]
+new_merged_data[, ":="(wt = ifelse(!is.na(wt), wt, normal_drifted_wt))]
+
+merged_data$Date = as.Date(merged_data$Date)
+merged_data[,.(Date, id, ret, meTotal, book)]
+
+apply_to_each_month = function(data) {
+  processed_data_list = list()
+  all_months = unique(month(data$Date))
+  
+  for (m in all_months) {
+    month_data = data[month(Date) == m]
+    month_data[, ":="(wt = meTotal/sum(meTotal, na.rm = TRUE)), by = Date]
+    month_data = month_data[,.(Date, id, ret, meTotal, book, wt)]
+    
+    merged_data_month = data[month(Date) == m]
+    merged_data_month = merge(merged_data_month, month_data[, .(Date, id, wt)], on = c("Date", "id"), all.x = TRUE)
+    merged_data_month[,.(Date, id, ret, meTotal, book, wt)]
+    merged_data_month[, ":="(lag_wt = lag(wt, 1)), by = id]
+    merged_data_month[, ":="(drifted_wt = lag_wt * (1 + ret))]
+    merged_data_month[, ":="(normal_drifted_wt = drifted_wt / sum(drifted_wt, na.rm = TRUE)), by = Date]
+    merged_data_month[, ":="(wt = ifelse(!is.na(wt), wt, normal_drifted_wt))]
+    
+    processed_data_list[[as.character(m)]] = merged_data_month
+  }
+  
+  processed_data = do.call(rbind, processed_data_list)
+  return(processed_data)
+}
+
+new_merged_data <- apply_to_each_month(merged_data)
